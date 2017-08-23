90c128d8ebb888be75539a6bb0e4a4157becc4b9
Muhammad Murad Ghaani Siddiqui <faizsiddiqui@live.com>
2017-08-23 19:52:47 +0500

    Add files via upload

---
+++ CraigslistScraper.py
@@ -0,0 +1,74 @@
+from urllib.request import urlopen as uReq
+from urllib.request import Request
+from bs4 import BeautifulSoup as soup
+
+
+def findCars():
+    minPrice = str(2500)
+    maxPrice = str(4000)
+    maxMiles = str(175000)
+
+    # secondPage = "s=120%"
+    pages = [120,240,360,480,600]
+
+    filename = "car_info.csv"
+
+    delPrev = open(filename, 'w')
+    delPrev.truncate(0)
+    delPrev.close()
+
+    for page in pages:
+        myURL = 'https://dallas.craigslist.org/search/cto?' + 's=' + str(page) + '&min_price=' + minPrice + '&max_price=' + maxPrice + '&max_auto_miles=' + maxMiles
+        req = Request(myURL, headers={'User-Agent':'Mozilla/5.0'})
+        uClient = uReq(myURL)
+        pageHTML = uClient.read()
+        uClient.close()     ### IMPORTANT ###
+
+        page_soup = soup(pageHTML, 'html.parser')
+
+        vehicle_list = page_soup.findAll('li', {'class':'result-row'})
+
+        cars = 0
+        # Creating a .csv file
+        file = open(filename, 'a')
+
+        headers = "Name, price, location, time, link\n"
+        file.write("Page: " + str(page) + "\n")
+        file.write("Search Criteria:\n" + "MinPrice: " + minPrice + "\nMaxPrice: " + maxPrice + "\nMaxMiles: " + maxMiles + "\n\n")
+        file.write(headers)
+        file.close()
+
+        for vehicle in vehicle_list:
+            car_info = open(filename, 'a')
+            # name
+            name = vehicle.p.a.text
+
+            # price
+            vehiclePrice = vehicle.p.findAll('span', {'class':'result-price'})[0].text
+            # location, some vehicles don't have a location
+            try:
+                vehicleLocation = vehicle.p.findAll('span', {'class':'result-hood'})
+                vehicleLocation = vehicleLocation[0].text
+
+            except:
+                vehicleLocation = 'location not given'
+
+            # time
+            timePosted = vehicle.p.time['datetime']
+
+            # link
+            siteName = 'https://dallas.craigslist.org/'
+            link = vehicle.a['href']
+            completeLink = siteName + link
+            cars += 1
+
+            car_info.write(name.replace(',','|') + "," + vehiclePrice + "," + vehicleLocation.replace(',','|') + "," + timePosted + "," + completeLink + "\n")
+            car_info.close()
+
+
+        # car_info.close()
+        print("page: " + str(page))
+
+findCars()
+
+
---
+++ README.txt
@@ -0,0 +1,2 @@
+This script scrapes https://dallas.craigslist.org/search/cto to find cars. It applies some filters and gives the result in a .csv file. 
+It uses beutiful soup to parse the HTML code.
